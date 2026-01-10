2e442cf1e578c512c02760a067094c58d25eba16
viseth-max <visethsopheach@gmail.com>
2026-01-10 12:45:25 +0700

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,50 @@ import Navbar from "./pages/Navbar";
 import Home from "./pages/Home";
 import Education from "./pages/Education";
 import Contact from "./pages/Contact";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Skill from "./pages/Skill";
 import Project from "./pages/Project";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
+  const routes = [
+    {
+      path: "/",
+      element: (
+        <>
+          <Home />
+          <Education />
+          <Skill />
+          <Project />
+          <Contact />
+        </>
+      ),
+    },
+    {
+      path: "/education",
+      element: <Education />,
+    },
+    {
+      path: "/skill",
+      element: <Skill />,
+    },
+    {
+      path: "/project",
+      element: <Project />,
+    },
+    {
+      path: "/contact",
+      element: <Contact />,
+    },
+  ];
+
   return (
     <Router>
       <Navbar />
       <main className="pt-0">
         <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <Home />
-                <Education />
-                <Skill />
-                <Project />
-                <Contact />
-              </>
-            }
-          />
-          <Route path="/education" element={<Education />} />
-          <Route path="/skill" element={<Skill />} />
-          <Route path="/project" element={<Project />} />
-          <Route path="/contact" element={<Contact />} />
+          {routes.map((route, index) => (
+            <Route key={index} path={route.path} element={route.element} />
+          ))}
         </Routes>
       </main>
     </Router>
